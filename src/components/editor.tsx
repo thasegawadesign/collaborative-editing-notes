4b13511bb616aaa813f340600c406a6c60238666
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-08-19 12:29:39 +0900

    fix: app url

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -14,7 +14,7 @@ export default function Editor() {
 
   const provider = new WebsocketProvider(
     process.env.NODE_ENV === 'production'
-      ? 'wss://https://collaborative-editing-notes'
+      ? 'wss://https://collaborative-editing-notes.gojiyuuniotorikudasai.com'
       : 'ws://localhost:3000',
     'my-roomname',
     doc
