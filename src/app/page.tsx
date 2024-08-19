1be38c8c7a1c37fee2b72729f6563b59602ebbe0
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-08-19 12:11:52 +0900

    feat: collaborative editing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import dynamic from 'next/dynamic';
+
+const Editor = dynamic(() => import('../components/editor'), { ssr: false });
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Editor />
+    </main>
+  );
 }
---
+++ src/components/editor.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import * as Y from 'yjs';
+
+import { getRandomUser } from '@/utils/randomUser';
+import '@blocknote/core/fonts/inter.css';
+import { BlockNoteView } from '@blocknote/mantine';
+import '@blocknote/mantine/style.css';
+import { useCreateBlockNote } from '@blocknote/react';
+import { WebsocketProvider } from 'y-websocket';
+
+export default function Editor() {
+  const doc = new Y.Doc();
+
+  const provider = new WebsocketProvider(
+    'ws://localhost:3000',
+    'my-roomname',
+    doc
+  );
+
+  const editor = useCreateBlockNote({
+    collaboration: {
+      provider,
+      fragment: doc.getXmlFragment('document-store'),
+      user: getRandomUser(),
+    },
+  });
+
+  return <BlockNoteView editor={editor} theme={'light'} />;
+}
